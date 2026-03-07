d9f775b3ceebbfb6a4d6efff2825bf01ee372646
RaposoBrendo <raposobrendo@gmail.com>
2026-03-07 11:55:20 -0300

    Beginning Interfaces

---
+++ src/application/ProgramTry1.java
@@ -0,0 +1,42 @@
+package application;
+
+import entities.Rent;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ProgramTry1 {
+    public static void main(String[] Args){
+
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter rental data");
+        System.out.print("Car model: ");
+        String carName = sc.nextLine();
+
+        System.out.print("Pickup (dd/MM/yyyy hh:mm): ");
+        LocalDateTime pickUp = LocalDateTime.parse(sc.nextLine(), fmt);
+
+        System.out.print("Return (dd/MM/yyyy hh:mm): ");
+        LocalDateTime returnCar = LocalDateTime.parse(sc.nextLine(), fmt);
+
+        System.out.print("Enter price per hour: ");
+        Double priceHour = sc.nextDouble();
+
+        System.out.print("Enter price per day: ");
+        Double priceDay = sc.nextDouble();
+
+        Rent rent = new Rent(carName, pickUp, returnCar, priceHour, priceDay);
+
+        System.out.println(rent);
+
+
+
+
+    }
+}
---
+++ src/entities/Rent.java
@@ -0,0 +1,117 @@
+package entities;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+public class Rent {
+
+    private String carName;
+    private LocalDateTime pickupCar;
+    private LocalDateTime returnCar;
+    private Double priceHour;
+    private Double priceDay;
+
+    public Rent (){
+
+    }
+
+    public Rent(String carName, LocalDateTime pickupCar, LocalDateTime returnCar, Double priceHour, Double priceDay) {
+        this.carName = carName;
+        this.pickupCar = pickupCar;
+        this.returnCar = returnCar;
+        this.priceHour = priceHour;
+        this.priceDay = priceDay;
+    }
+
+    public String getCarName() {
+        return carName;
+    }
+
+    public void setCarName(String carName) {
+        this.carName = carName;
+    }
+
+    public LocalDateTime getPickupCar() {
+        return pickupCar;
+    }
+
+    public void setPickupCar(LocalDateTime pickupCar) {
+        this.pickupCar = pickupCar;
+    }
+
+    public LocalDateTime getReturnCar() {
+        return returnCar;
+    }
+
+    public void setReturnCar(LocalDateTime returnCar) {
+        this.returnCar = returnCar;
+    }
+
+    public Double getPriceHour() {
+        return priceHour;
+    }
+
+    public void setPriceHour(Double priceHour) {
+        this.priceHour = priceHour;
+    }
+
+    public Double getPriceDay() {
+        return priceDay;
+    }
+
+    public void setPriceDay(Double priceDay) {
+        this.priceDay = priceDay;
+    }
+
+    public int billedHours(){
+        Duration duration = Duration.between(pickupCar, returnCar);
+
+        return (int) Math.ceil(duration.toMinutes()/60.0);
+    }
+
+    public double basicPayment(){
+
+        int hours = billedHours();
+
+        if(hours <= 12){
+            return priceHour * hours;
+        }
+        else{
+            int days = (int) Math.ceil(hours/24.0);
+            return priceDay * days;
+        }
+    }
+
+    public Double tax(){
+
+        double basic = basicPayment();
+
+        if(basic <= 100){
+            return basic * 0.2;
+        }
+        else {
+            return basic * 0.15;
+        }
+    }
+
+    public Double totalPayment(){
+        return basicPayment() + tax();
+    }
+
+    @Override
+    public String toString(){
+
+        double basic = basicPayment();
+        double tax = tax();
+        double total = basic + tax;
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("INVOICE:\n");
+        sb.append("Basic payment: " + String.format("%.2f%n", basic));
+        sb.append("TAX: " + String.format("%.2f%n", tax));
+        sb.append("Total payment: " + String.format("%.2f%n", total));
+        return sb.toString();
+    }
+
+
+}
